cba5d8c5190d68272654229b1f9ec9c0ed15d171
Tran Minh Hieu <minhhieu.tran0407@gmail.com>
2020-08-20 23:36:55 +0700

    write SendEmailController

--- src/main/java/hust/hieutm/controller/SendEmailController.java
+++ src/main/java/hust/hieutm/controller/SendEmailController.java
@@ -1,5 +1,9 @@
 package hust.hieutm.controller;
 
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +13,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SendEmailController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     }
@@ -16,10 +21,36 @@ public class SendEmailController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        String email = req.getParameter("email");
+        String toEmail = req.getParameter("email");
         String subject = req.getParameter("subject");
         String message = req.getParameter("message");
 
-        
+        String myEmail = "minhhieu.tran0407@gmail.com";
+        String myPassword ="020304";
+
+        try{
+            Email email = new SimpleEmail();
+
+            email.setHostName("smtp.googlemail.com");
+            email.setSmtpPort(456);
+            email.setAuthentication(myEmail, myPassword);
+
+            email.setSSLOnConnect(true);
+            // Người gửi
+            email.setFrom(myEmail);
+
+            // Tiêu đề
+            email.setSubject(subject);
+
+            // Nội dung email
+            email.setMsg(message);
+
+            // Người nhận
+            email.addTo(toEmail);
+            email.send();
+            System.out.println("Sent!!");
+        } catch (EmailException exception){
+            exception.printStackTrace();
+        }
     }
 }
